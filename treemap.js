7ec9522bb378766875b66bddf535b1cc1062b085
federico_laggiard <federico.laggiard@spindox.it>
2014-10-16 11:50:18 +0200

    - added zoomable treemap

--- treemap.js
+++ treemap.js
@@ -1,3 +1,71 @@
 /**
  * Created by federicolaggiard on 14/10/14.
  */
+
+var baseTreemap = {
+
+    renderDivId: '',                      //Rendering div. Must be set before drawing call
+    width: 500,                           //Width and height of the canvas
+    height: 500,
+    canvas: null,                         //Reference to the drawing canvas
+    curInputUrl: '',                      //Url used to fetch JSON. Must be set before drawing call
+    curData: null,                        //Current data loaded into the treemap
+    treemap: null,                        //Ref to the current treemap
+    cells: null,                          //Ref to svg group containing cells
+    color: d3.scale.category10(),         //D3 color scale used by the treemap
+
+    /***
+     * Method to start drawing process of treemap
+     * @param inputUrl Url for fetching data
+     */
+    draw: function(inputUrl){
+
+        if( !this.renderDivId ) return console.error("NO RENDER DIV ID");
+        if( !inputUrl ) return console.error("NO JSON URL PROVIDED");
+
+        //Creating svg canvas
+        this.canvas = d3.select(this.renderDivId)
+            .append("svg")
+            .attr("width",this.width)
+            .attr("height",this.height);
+
+        //storing called URL
+        this.curInputUrl = inputUrl;
+
+        //Call JSON with D3 method
+        var that = this;
+        d3.json(this.curInputUrl, function(data){
+
+            //storing current data
+            that.curData = data;
+
+            //start drawing treemap
+            that.treemap = d3.layout.treemap()
+                .size([that.width,that.height])//setting size == to canvas size. TODO: padding?
+                .nodes(that.curData)//binding data source
+            ;
+
+            //create all groups that contains the cells, appending a g for each data element
+            that.cells = that.canvas.selectAll(".cell")
+                .data(that.treemap)
+                .enter()
+                    .append("g")
+                    .attr("class","cell")
+            ;
+
+            //append to each group a rectangle, starting at position x,y
+            //and with dimension dx,dy those values are already provided by d3.treemap in each dataItem (element of data)
+            that.cells.append("rect")
+                .attr("x",function(dataItem){ return dataItem.x; })
+                .attr("y",function(dataItem){ return dataItem.y; })
+                .attr("width",function(dataItem){return dataItem.dx; })
+                .attr("height",function(dataItem){return dataItem.dy; })
+                //apply a color for each group using the scale color based on the parent (so leaf from the same parent shares colors)
+                //that after checking if the cur item has children (so only the leaf)
+                .attr("fill", function(d){ return d.children ? null : that.color(d.parent.name) })
+            ;
+
+        })
+
+    }
+};
---
+++ zoomTreemap.js
@@ -0,0 +1,150 @@
+/**
+ * Created by federicolaggiard on 16/10/14.
+ */
+
+var zoomTreemap = {
+
+    renderDivId: 'treemap',               //Rendering div. Must be set before drawing call
+    width: 831,                           //Width and height of the canvas
+    height: 340,
+    canvas: null,                         //Reference to the drawing canvas
+    curData: null,                        //Current data loaded into the treemap
+    treemap: null,                        //Ref to the current treemap
+    cells: null,                          //Ref to svg group containing cells
+    color: d3.scale.category20(),         //D3 color scale used by the treemap
+
+    draw: function(data){
+
+        var w = this.width,
+            h = this.height,
+            x = d3.scale.linear().range([0, w]),
+            y = d3.scale.linear().range([0, h]),
+            color = this.color,
+            root,
+            node
+            ;
+
+        this.treemap = d3.layout.treemap()
+            .round(false)
+            .size([w, h])
+            .sticky(true)
+            .value(function (d) {
+                return d.size + 20;
+            })
+            .sort(function(a,b) {
+                return b.value - a.value;
+            });
+
+        this.canvas = d3.select('#' + this.renderDivId)
+            .attr("class", "chart")
+            .style("width", w + "px")
+            .style("height", h + "px")
+            .append("svg:svg")
+            .attr("width", w)
+            .attr("height", h)
+            .append("svg:g")
+            .attr("transform", "translate(.5,.5)");
+
+        node = root = data;
+
+        var nodes = this.treemap.nodes(root)
+            .filter(function (d) {
+                return !d.children;
+            });
+
+        var cells = this.canvas.selectAll("g")
+            .data(nodes)
+            .enter().append("svg:g")
+            .attr("class", "cell")
+            .attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            })
+            .on("click", function (d) {
+                return zoom(node == d.parent ? root : d.parent);
+            });
+
+        cells.append("svg:rect")
+            .attr("width", function (d) {
+                return d.dx - 1;
+            })
+            .attr("height", function (d) {
+                return d.dy - 1;
+            })
+            .style("fill", function (d) {
+                //return color(d.parent.name);
+                return 'rgb(66, 161, 201)';
+            });
+
+        cells.append("svg:text")
+            .attr("x", function (d) {
+                return d.dx / 2;
+            })
+            .attr("y", function (d) {
+                return d.dy / 2;
+            })
+            .attr("dy", ".35em")
+            .attr("text-anchor", "middle")
+            .text(function (d) {
+                return d.name;
+            })
+            .style("opacity", function (d) {
+                d.w = this.getComputedTextLength();
+                return d.dx > d.w ? 1 : 0;
+            });
+
+        d3.select(window).on("click", function () {
+            zoom(root);
+        });
+
+//        d3.select("select").on("change", function () {
+//            this.treemap.value(this.value == "size" ? size : count).nodes(root);
+//            zoom(node);
+//        });
+
+        function size(d) {
+            return d.size + 300;
+        }
+
+        function count(d) {
+            return 1;
+        }
+
+        var that = this;
+        function zoom(d) {
+            var kx = w / d.dx, ky = h / d.dy;
+            x.domain([d.x, d.x + d.dx]);
+            y.domain([d.y, d.y + d.dy]);
+
+            var t = that.canvas.selectAll("g.cell").transition()
+                .duration(d3.event.altKey ? 7500 : 750)
+                .attr("transform", function (d) {
+                    return "translate(" + x(d.x) + "," + y(d.y) + ")";
+                });
+
+            t.select("rect")
+                .attr("width", function (d) {
+                    return kx * d.dx - 1;
+                })
+                .attr("height", function (d) {
+                    return ky * d.dy - 1;
+                });
+
+            t.select("text")
+                .attr("x", function (d) {
+                    return kx * d.dx / 2;
+                })
+                .attr("y", function (d) {
+                    return ky * d.dy / 2;
+                })
+                .style("opacity", function (d) {
+                    return kx * d.dx > d.w ? 1 : 0;
+                });
+
+            node = d;
+            d3.event.stopPropagation();
+        }
+
+
+    }
+
+};
